f5348d9b71e3db959695216614010abafb031665
Anusha Srirenganathan <anusha25cse@gmail.com>
2021-12-16 12:37:54 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'STCenter Documents'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Anusha'
 
 release = '0.1'
 version = '0.1.0'
